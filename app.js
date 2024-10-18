acc3b9ee68b5472b587f1c77293fa15edf2d75c3
Nyukaa <annashitikova@gmail.com>
2024-10-18 15:42:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-nnn
+const handleRequest = (request) => {
+  const url = new URL(request.url);
+  const params = url.searchParams;
+  let message = "Invalid parameters"
+  const operation = params.get("operation")
+
+  const number1 = Number(params.get("number1"));
+  const number2 = Number(params.get("number2"));
+  if (operation === "product"){
+    message = number1*number2;
+  } 
+  else if (operation === "sum") {
+    message = number1+number2;
+  }
+
+  return new Response(message);
+ 
+};
+
+Deno.serve(handleRequest);
